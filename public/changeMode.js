b15030d2ea9420386a017bdfa13107bed711acee
Abrorjon Asralov <asralov@arizona.edu>
2023-12-08 01:25:19 -0700

    finished the stylign of the web

--- public/changeMode.js
+++ public/changeMode.js
@@ -1,38 +1,30 @@
-function changeBorders(color){
-    const textBox = document.getElementsByClassName('textBox');
-    for (let i = 0; i < textBox.length; i++){
-        textBox[i].style.borderColor = color;
-        changeHR(color);
-    }
-}
-
-function changeHR(color){
-    let hrBox = document.getElementsByTagName('hr');
-    for (let i = 0; i < hrBox.length; i++){
-        hrBox.style.backgroundColor = color;
-    }
-   
-}
-
-
 // using darkMode to be true since the default mode is dark
 let darkMode = true;
 const btn = document.getElementById("changeModeBtn");
-const con = document.getElementById("content");
-function changeMode(){
-    // if it is not a dark mode and user wants a black mode
-    if (!darkMode){
-        document.body.style.backgroundColor = "#000";
-        document.body.style.color = "green";
-        changeBorders("green");
-        // then updating that it is not a light mode anymore
-        darkMode = true;
-    } else {
-        // else case when it is a dark mode and user wants a light mode
-        document.body.style.backgroundColor = "#fff";
-        document.body.style.color = "black";
-        changeBorders("black");
-        darkMode = false;
+
+function changeMode() {
+    // Toggle the darkMode variable
+    darkMode = !darkMode;
+
+    // Update body background and text color
+    document.body.style.backgroundColor = darkMode ? "#444" : "#bbb";
+    document.body.style.color = darkMode ? "#fff" : "#000"; // Adjust text color here
+
+    // Update border color and background color for .textBox elements
+    const textBox = document.getElementsByClassName('textBox');
+    for (let i = 0; i < textBox.length; i++) {
+        textBox[i].style.borderColor = darkMode ? "#fff" : "#000"; // Adjust border color here
+
+        // Update background color with linear gradient
+        textBox[i].style.background = darkMode
+            ? "linear-gradient(#111, #333, #111)"
+            : "linear-gradient(#ccc, #fff, #ccc)";
+    }
+
+    // Update hr elements background color
+    const hrBox = document.getElementsByTagName('hr');
+    for (let i = 0; i < hrBox.length; i++) {
+        hrBox[i].style.backgroundColor = darkMode ? "#fff" : "#000"; // Adjust hr background color here
     }
 }
 
